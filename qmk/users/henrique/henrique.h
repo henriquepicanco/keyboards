047828b19310d24985a0cc3dd1b272667453cc88
Henrique Picanço <114828539+henriquepicanco@users.noreply.github.com>
2026-07-22 20:03:27 -0300

    feat(qmk): add user configurations

---
+++ qmk/users/henrique/henrique.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "quantum.h"
+
+#define MT_A    LGUI_T(KC_A)
+#define MT_S    LALT_T(KC_S)
+#define MT_D    LCTL_T(KC_D)
+#define MT_F    LSFT_T(KC_F)
+
+#define MT_J    RSFT_T(KC_J)
+#define MT_K    RCTL_T(KC_K)
+#define MT_L    RALT_T(KC_L)
+#define MT_SCLN RGUI_T(KC_SCLN)
+
